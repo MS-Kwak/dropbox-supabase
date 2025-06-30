5a48800181aeb07511fd491100b1ceee4ff04718
MS-Kwak <kkminseo@gmail.com>
2025-06-30 23:52:47 +0900

    드롭박스 세팅 init

---
+++ app/ui.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function UI() {
+  return (
+    <main>
+      <h1>Welcome to the UI Component</h1>
+      <p>This is a placeholder for your UI component.</p>
+    </main>
+  );
+}
